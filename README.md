42b6965c9a680aa5ae43c606e303e09e689cdbb7
Liam Barkley <Liam.Barkley@bbd.co.za>
2025-07-29 12:20:41 +0200

    fedify scaffolding and google auth login

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # InstaMastadonGram
+
+Set the following environment variables:
+
+- GOOGLE_CLIENT_ID
+- GOOGLE_CLIENT_SECRET
+- GOOGLE_CALLBACK_URL
+- MONGO_URI
+- SESSION_SECRET
+
+## To run client
+
+```
+npm run dev/prod
+```
+
+## Postman: Test Login
+
+```
+http://localhost:8000/auth/google
+```
+
+## Postman: Test Fedify Endpoint
+
+1. **Set the request URL:**
+
+```
+GET http://localhost:8000/users/test
+```
+
+2. **Add the HTTP Header:**
+
+| Key    | Value                     |
+|--------|---------------------------|
+| Accept | application/activity+json |
+
+3. **Send the request**
+
+You should receive a JSON response describing the user as an ActivityPub `Person`.
---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import session from "express-session";
+import passport from "passport";
+import authRoutes from "./routes/auth.routes.ts";
+import federation from "./services/federation.ts";
+import mongoose from "mongoose";
+import { integrateFederation } from "@fedify/express";
+import "./config/passport";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET!,
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRoutes);
+
+app.use(integrateFederation(federation, (req) => undefined));
+
+app.get("/", (req, res) => {
+  res.send("Hello, Fedify + Google Auth!");
+});
+
+export default app;
