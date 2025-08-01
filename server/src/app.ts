0219de056825ff158a9cb4f5e8e852907cbcf54d
KeloDraken <65687571+KeloDraken@users.noreply.github.com>
2025-08-01 08:25:30 +0000

    Apply backend formatting changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,16 +15,18 @@ const app = express();
 app.set("trust proxy", true);
 app.use(express.json());
 
-app.use(session({
-  secret: process.env.SESSION_SECRET!,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URI!,
-    touchAfter: 24 * 3600,
-    ttl: 14 * 24 * 60 * 60
-  })
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI!,
+      touchAfter: 24 * 3600,
+      ttl: 14 * 24 * 60 * 60,
+    }),
+  }),
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
