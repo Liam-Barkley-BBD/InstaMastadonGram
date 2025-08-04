1e98ded9cf34ced6c93052363f74af78bcec270a
Vuyo Mama <joy@bbd.co.za>
2025-08-04 19:11:32 +0200

    Remove old terraform

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "instamastadongram-terraform-state"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-    acl     = "bucket-owner-full-control"
-  }
-}
